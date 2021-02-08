937d5dce797ecc00927685542efb27e138d00ed9
Abhinav Sharma <abhinav@laso.ai>
2021-02-07 19:31:17 -0600

    Update barrel_roll.js

--- barrel_roll.js
+++ barrel_roll.js
@@ -1,7 +1,7 @@
 
 
 (() => {
-    var doABarrelRoll = function(){var a="-webkit-",b='transform:rotate(1turn);',c='transition:3s;';document.head.innerHTML+='<style>body{'+a+b+a+c+b+c}
+    var doABarrelRoll = function(){var a="-webkit-",b='transform:rotate(1turn);',c='transition:4s;';document.head.innerHTML+='<style>body{'+a+b+a+c+b+c}
 
     if (document.readyState === 'complete' || document.readyState === 'interactive') {
       doABarrelRoll();
