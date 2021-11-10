793f1e82d150e77cbf8d2c7ca1ea0ec9d89448f3
Felipe Almeida <felipewill2@gmail.com>
2021-11-10 15:48:42 -0300

    Update probable_autoclick.js

--- probable_autoclick.js
+++ probable_autoclick.js
@@ -1,5 +1,5 @@
 (() => {
-  const COOKIES_MODAL_MAX_HEIGHT = 500.0
+  const COOKIES_MODAL_MIN_HEIGHT = 100.0;
 
   const buildSelector = (element) => {
     let currentElement = element;
@@ -34,12 +34,9 @@
   };
 
   const clickElement = (el, selector, tryCount) => {
-    el.click()
-  
+    el.click();
     // If element still exists maybe it did not work correctly, try again
-    if (document.querySelector(selector) && tryCount < 3) {
-      setTimeout(() => clickElement(el, selector, tryCount + 1), 250);
-    }
+    setTimeout(() => document.querySelector(selector) && tryCount < 3 && clickElement(el, selector, tryCount + 1), 250);
   };
   
   const hasFormAncestor = (element) => {
@@ -64,29 +61,39 @@
     }
   
     const mustHaveWords = [ 'ok', 'accept', 'yes', 'continue', 'agree' ];
-  
+
     // Since we don't know the order of the button we are testing in the modal
     // Let's look for the ones with positive words
     const innerText = button.innerText.toLocaleLowerCase();
-    if (!mustHaveWords.some((word) => innerText.includes(word))) {
+    
+    if (!mustHaveWords.some((word) => innerText.match(`\\b${ word }\\b`))) {
       return null;
     }
   
-    let foundCookies = false;
-    let isModal = false;
-    let parent = button.parentElement;
-  
-    while (parent) {
-      const innerText = parent.innerText.toLocaleLowerCase();
-      foundCookies = innerText.includes('cookie') || innerText.includes('cookies');
-      isModal = parent.clientHeight <= COOKIES_MODAL_MAX_HEIGHT;
-  
-      parent = parent.parentElement;    
-  
-      if (!isModal || parent === document.body) { break; }
-    }
-  
-    return foundCookies && isModal ? button : null;
+    const highestParent = () => {
+      let parent = button.parentElement;
+
+      if (parent === document.body) {
+        return null;
+      }
+
+      while (parent) {
+        if (!parent.parentElement ||
+            parent.parentElement === document.body ||
+            parent.parentElement.clientHeight === 0) { break; }
+
+        parent = parent.parentElement;
+      }
+
+      return parent;
+    };
+
+    const parent = highestParent();
+    const parentInnerText = parent.innerText.toLocaleLowerCase();
+    const foundCookies = parentInnerText.includes('cookie') || parentInnerText.includes('cookies');
+    const hasEnoughSize = parent.clientHeight >= COOKIES_MODAL_MIN_HEIGHT;
+    
+    return foundCookies && hasEnoughSize ? button : null;
   };
   
   const run = () => {
@@ -107,37 +114,41 @@
       clickElement(button, selector, 0);
     };
 
-    buildRuleAndClick(checkButtonsIn(document.body));
+    const possibleButton = checkButtonsIn(document.body);
 
-    const observer = new MutationObserver((mutationsList) => {
-      const findPossibleCookie = () => {
-        for(const mutation of mutationsList) {
-          if (mutation.type === 'childList') {
-            const nodes = Array.from(mutation.addedNodes);
-  
-            for (const node of nodes) {
-              if (node instanceof HTMLButtonElement && isPossibleAcceptCookies(node)) {
-                
-                return node;
-              } else if (node.nodeType == Node.ELEMENT_NODE) {
-                checkButtonsIn(node);
+    if (possibleButton) {
+      buildRuleAndClick(possibleButton);
+    } else {
+      const observer = new MutationObserver((mutationsList) => {
+        const findPossibleCookie = () => {
+          for(const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+              const nodes = Array.from(mutation.addedNodes);
+    
+              for (const node of nodes) {
+                if (node instanceof HTMLButtonElement && isPossibleAcceptCookies(node)) {
+                  
+                  return node;
+                } else if (node.nodeType == Node.ELEMENT_NODE) {
+                  checkButtonsIn(node);
+                }
               }
             }
           }
         }
-      }
-  
-      const button = findPossibleCookie();
-  
-      if (button) {
-        buildRuleAndClick(button);
-        observer.disconnect();
-      }
-    });
-  
-    observer.observe(document, { childList: true, subtree: true });
-  
-    setTimeout(() => observer.disconnect(), 10 * 1000);
+    
+        const button = findPossibleCookie();
+        
+        if (button) {
+          buildRuleAndClick(button);
+          observer.disconnect();
+        }
+      });
+    
+      observer.observe(document, { childList: true, subtree: true });
+    
+      setTimeout(() => observer.disconnect(), 10 * 1000);
+    }
   };
   
   run();
